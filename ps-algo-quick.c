b10aa416bc1fa85f00135daf370fb6016d53808d
gris <185072671+d-branco@users.noreply.github.com>
2025-02-02 15:57:32 +0000

    alignment

--- ps-algo-quick.c
+++ ps-algo-quick.c
@@ -6,26 +6,35 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 14:57:08 by abessa-m          #+#    #+#             */
-/*   Updated: 2025/02/02 14:54:40 by abessa-m         ###   ########.fr       */
+/*   Updated: 2025/02/02 15:57:10 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	economic_operation(t_ps *ps);
-static void	move_to_b(t_ps *ps);
+static t_eco	economic_operation(t_ps *ps);
+static void		move_to_b(t_ps *ps);
+static void		align_stacks(t_ps *ps, t_eco eco);
+static void		align_stacks_direction(t_ps *ps, t_eco eco);
 
 int	algo_quick(t_ps *ps)
 {
+	t_eco	eco;
+
 	move_to_b(ps);
 	algo_small(ps);
-	economic_operation(ps);
+	while (stack_len(ps, 'b') > 0)
+	{
+		eco = economic_operation(ps);
+		align_stacks(ps, eco);
+	}
+	algo_buble(ps, 'a');
 	return (0);
 }
 
 //	int	maximum_bellow(t_ps *ps, char chr, int border);
 //	int	minimum_above(t_ps *ps, char chr, int border);
-static void	economic_operation(t_ps *ps)
+static t_eco	economic_operation(t_ps *ps)
 {
 	t_eco	eco;
 
@@ -42,7 +51,61 @@ static void	economic_operation(t_ps *ps)
 		else
 			ft_printf("    cost: %i step. Reverse: no\n", eco.cost);
 	}
-	return ;
+	return (eco);
+}
+
+static void	align_stacks(t_ps *ps, t_eco eco)
+{
+	int	steps_a;
+	int	steps_b;
+
+	if (eco.reverse_direction)
+	{
+		steps_a = (stack_len(ps, 'a') - eco.next_a) + 1;
+		steps_b = 1 + stack_len(ps, 'b') - get_pos_from_value(ps, 'b',
+				minimum_above(ps, 'b', get_value_from_pos(ps, 'a',
+						eco.next_a)));
+		while ((steps_a > 0) || (steps_b > 0))
+		{
+			if ((steps_a > 0) && (steps_b > 0))
+				reverse_rr(ps);
+			else if (steps_a > 0)
+				reverse_a(ps);
+			else if (steps_b > 0)
+				reverse_b(ps);
+			steps_a--;
+			steps_b--;
+		}
+	}
+	else
+		align_stacks_direction(ps, eco);
+	push_a(ps);
+}
+
+static void	align_stacks_direction(t_ps *ps, t_eco eco)
+{
+	int	steps_a;
+	int	steps_b;
+
+	steps_a = 0 - 1 + eco.next_a;
+	steps_b = 0 - 1 + get_pos_from_value(ps, 'b',
+			minimum_above(ps, 'b', get_value_from_pos(ps, 'a', eco.next_a)));
+	while ((steps_a > 0) || (steps_b > 0))
+	{
+		if (ps->verbose)
+		{
+			ft_printf("stack a needs to rotate %i times\n", steps_a);
+			ft_printf("stack b needs to rotate %i times\n", steps_b);
+		}
+		if ((steps_a > 0) && (steps_b > 0))
+			rotate_rr(ps);
+		else if (steps_a > 0)
+			rotate_a(ps);
+		else if (steps_b > 0)
+			rotate_b(ps);
+		steps_a--;
+		steps_b--;
+	}
 }
 
 static void	move_to_b(t_ps *ps)
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 11:35:47 by abessa-m          #+#    #+#             */
-/*   Updated: 2025/02/01 15:10:52 by abessa-m         ###   ########.fr       */
+/*   Updated: 2025/02/02 15:49:34 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv)
 	int		verbose;
 	t_ps	ps;
 
-	verbose = 1;
+	verbose = 0;
 	if (argc == 1)
 		return (1);
 	if (initialize(&ps, argc, argv, verbose) != 0)
