9a7f1d91e45c3a9d5e9bc2fb431e670c83543a22
d-cluster <abessa-m@c1r2s7.42porto.com>
2025-02-04 15:26:52 +0000

    5300

--- ps-algo-quick-move.c
+++ ps-algo-quick-move.c
@@ -6,7 +6,7 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 20:49:20 by abessa-m          #+#    #+#             */
-/*   Updated: 2025/02/04 13:01:03 by abessa-m         ###   ########.fr       */
+/*   Updated: 2025/02/04 15:24:47 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,44 +35,34 @@ void	move_to_b(t_ps *ps)
 
 void	quick_move(t_ps *ps)
 {
-	int	divisions;
 	int	len;
-	//int	max;
-	//int	median_value;
-	int	j;
 
+//len
+//24	5725
+//23	5715
+//22	5681
+//21	5780
 
-//8		6690
-//12			5743
-
-//16			5515
-//18				5415
-//20				5498
-//24			5762
-//32	6487
-	divisions = 4;
+	len = 12;
 	if (stack_len(ps, 'a') > 225)
-		divisions = 18;
-	len = stack_len(ps, 'a') / divisions;
-	j = 0;
-	while ((stack_len(ps, 'a') > 3))
+		len = 22;
+	if (ps->verbose)
+		ft_printf("\033[34mMoving to stack b\033[0m\n");
+	while (stack_len(ps, 'a') > 2)
 	{
-		//median_value = find_nth_smallest(ps, 'a', len/2);
-		//max = find_nth_smallest(ps, 'a', len);
-		if (ps->verbose)
-			ft_printf("\033[34mMoving to stack b\033[0m\n");
-		while (stack_len(ps, 'a') >= 3)
+		if (get_value_from_pos(ps, 'a', 1) < find_nth_smallest(ps, 'a', len))
 		{
-			if (get_value_from_pos(ps, 'a', 1) < find_nth_smallest(ps, 'a', len))
+			push_b(ps);
+			if (get_value_from_pos(ps, 'b', 1) > find_nth_smallest(ps, 'a', len / 4))
 			{
-				push_b(ps);
-				if (get_value_from_pos(ps, 'b', 1) > find_nth_smallest(ps, 'a', len/2))
+				if (get_value_from_pos(ps, 'a', 1) >= find_nth_smallest(ps, 'a', len))
+					rotate_rr(ps);
+				else
 					rotate_b(ps);
 			}
-			else
-				rotate_a(ps);
 		}
-		j++;
+		else
+			rotate_a(ps);
 	}
 }
 
