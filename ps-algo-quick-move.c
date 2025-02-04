23ac6513743e65210b33be086e3d2867965a82a3
gris <185072671+d-branco@users.noreply.github.com>
2025-02-04 12:47:31 +0000

    5515

--- ps-algo-quick-move.c
+++ ps-algo-quick-move.c
@@ -6,7 +6,7 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 20:49:20 by abessa-m          #+#    #+#             */
-/*   Updated: 2025/02/04 09:16:16 by abessa-m         ###   ########.fr       */
+/*   Updated: 2025/02/04 12:45:10 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,34 +37,34 @@ void	quick_move(t_ps *ps)
 {
 	int	divisions;
 	int	len;
-	int	max;
-	int	median_value;
-	int	end;
+	//int	max;
+	//int	median_value;
 	int	j;
 
+
+//8		6996
+//16	6981
 	divisions = 4;
 	if (stack_len(ps, 'a') > 225)
-		divisions = 8;
+		divisions = 16;
 	len = stack_len(ps, 'a') / divisions;
 	j = 0;
-	while ((j < divisions - 1) || (stack_len(ps, 'a') > 3))
+	while ((stack_len(ps, 'a') > 3))
 	{
-		median_value = find_nth_smallest(ps, 'a', len/2);
-		max = find_nth_smallest(ps, 'a', len);
+		//median_value = find_nth_smallest(ps, 'a', len/2);
+		//max = find_nth_smallest(ps, 'a', len);
 		if (ps->verbose)
 			ft_printf("\033[34mMoving to stack b\033[0m\n");
-		end = stack_len(ps, 'a');
-		while ((end > 0) && (stack_len(ps, 'a') >= 3))
+		while (stack_len(ps, 'a') >= 3)
 		{
-			if (get_value_from_pos(ps, 'a', 1) < max)
+			if (get_value_from_pos(ps, 'a', 1) < find_nth_smallest(ps, 'a', len))
 			{
 				push_b(ps);
-				if (get_value_from_pos(ps, 'b', 1) > median_value)
+				if (get_value_from_pos(ps, 'b', 1) > find_nth_smallest(ps, 'a', len/2))
 					rotate_b(ps);
 			}
 			else
 				rotate_a(ps);
-			end--;
 		}
 		j++;
 	}
